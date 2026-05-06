e0fd808221641f4b9fa2d6ef6f4b84a91f9c0fc0
fa24hee <146456205+fa24hee@users.noreply.github.com>
2026-05-06 20:38:02 +0600

    Add files via upload

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
-"use strict";
-module.exports = require("./plugin");
+export * from "./enums.js";
+export * from "./modifiers/index.js"; // eslint-disable-next-line import/no-unused-modules
+
+export { popperGenerator, detectOverflow, createPopper as createPopperBase } from "./createPopper.js"; // eslint-disable-next-line import/no-unused-modules
+
+export { createPopper } from "./popper.js"; // eslint-disable-next-line import/no-unused-modules
+
+export { createPopper as createPopperLite } from "./popper-lite.js";
